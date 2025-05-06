bfba20571b53e6ae7043f1a0e199bfe84c1fb075
Gustavo-m72 <gustavomstefanello72@gmail.com>
2025-05-06 20:33:30 -0300

    estilo de login e Rotas

--- fichadetreinos/urls.py
+++ fichadetreinos/urls.py
@@ -3,14 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('aluno/<int:aluno_id>/', views.aluno, name='aluno'),
-    path('professor/', views.professor, name='professor'),
     path('treino/<int:aluno_id>/', views.treino, name='treino'),
-    path('cadastrartreino/<int:aluno_id>', views.cadastrartreino, name='cadastrartreino'),
-    path('excluirtreino/<int:treino_id>/', views.excluirtreino, name='excluirtreino'),
+    path('cadastrartreino/<int:aluno_id>/', views.cadastrartreino, name='cadastrartreino'),
     path('cadastrarexercicio/', views.cadastrarexercicio, name='cadastrarexercicio'),
 ]
